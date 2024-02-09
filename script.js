e2b3408f428a31b8af021dd8c35a1a8038730a7a
s27144 <s27144@pjwstk.edu.pl>
2024-02-09 15:32:42 +0100

    essa
    hell yeah

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-
-  document.getElementById('tryNowButton').addEventListener('click', function() {
-    const cvHeader = document.getElementById('cvHeader');
-    const cvForm = document.getElementById('cvForm');
-    cvForm.style.display = 'block';
-  });
-// Efekt "pisania"
-const text = "Stwórz swoje CV z CV Creator już teraz!";
-let index = 0;
-let displayText = '';
-
-function typeWriter() {
-  if (index < text.length) {
-    displayText += text.charAt(index);
-    document.getElementById("typingHeader").innerHTML = displayText;
-    index++;
-    setTimeout(typeWriter, 100); // Przerwa między pojawianiem się kolejnych liter
-  } else {
-    index = 0; // Zresetowanie indeksu, aby animacja zaczęła się od nowa
-    displayText = ''; // Zresetowanie wyświetlanego tekstu
-    setTimeout(typeWriter, 500); // Opóźnienie przed ponownym rozpoczęciem animacji
-  }
+function show() {
+    document.getElementById("valentine").style.display = "block";
+    document.getElementById("response-buttons").style.display = "flex";
+    document.getElementById("message").style.display = "none";
+	document.getElementById("yes").style.display = "block";
+	document.getElementById("no").style.display = "block";
 }
 
-// Rozpoczęcie efektu pisania po załadowaniu strony
-window.onload = function() {
-  typeWriter();
-};
+function answer(response) {
+    const yesButton = document.getElementById("yes");
+    if (response === 'no') {
+        // Pobieranie aktualnej skali i zwiększanie jej o 10%
+        const currentScale = Number(yesButton.style.transform.replace('scale(', '').replace(')', '')) || 1;
+        const newScale = currentScale * 1.1;
+        yesButton.style.transform = `scale(${newScale})`;
+    } else if (response === 'yes') {
+        // Dodanie marginesu do filmu, aby zapewnić przestrzeń
+        const video = '<div style="margin-top: 20px;"><iframe width="560" height="315" src="https://www.youtube.com/embed/DrXSXYfYV0o?si=w2Nd2jIJiHI--3Ii" frameborder="0" allowfullscreen></iframe></div>';
+        document.getElementById("response-buttons").insertAdjacentHTML('afterend', video);
+		document.getElementById("end").style.display = "block";
+		document.getElementById("end2").style.display = "block";
+    }
+}
